f992302a41ed00b0e302026e21eba94126975afd
davidabutler92 <davidabutler92@yahoo.com>
2020-08-21 12:07:50 -0700

    add userResults stored in variable, use calNums function on userInput and console.log()

--- js/main.js
+++ js/main.js
@@ -3,26 +3,25 @@ $(document).ready(function() {
   $("#robogersForm").submit(function(event) {
     event.preventDefault();
     const userInput = $("#userInput").val();
-    console.log(userInput);
+    // console.log(userInput);
 
 
 //Business logic
   const calNums = userInput => {
-    let array = [];
+    const array = [];
     for(let i = 0; i <= userInput; i++) {
       arrInput = i;
       if(arrInput.toString().includes("3")) {
         array.push("won't you be my neighbor");
       } else if(arrInput.toString().includes("2")) {
-        array.push("beep");
+        array.push("boop");
       }
     }
     console.log(array);
-    return(array);
+    
   }
 
-
-
-
+    const userResults = calNums(userInut);
+    console.log(userResults);
   });
 });
